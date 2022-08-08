373d9c01feadee5100084c9dfe517812197068cc
Thái Thành Nam <74032428+thanhnam2811@users.noreply.github.com>
2022-08-08 15:14:21 +0700

    basic routing

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Template engine
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "/resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
